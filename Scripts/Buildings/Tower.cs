bd490b25fff58b5010505f88f135e3c7a005d9a3
Artak <jarzop@gmail.com>
2023-11-25 00:50:36 +0400

    Created project's sceleton

---
+++ Scripts/Buildings/Tower.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private Transform _horizontalRotationObject;
+    [SerializeField] private Transform _verticalRotationObject;
+    
+    
+}
